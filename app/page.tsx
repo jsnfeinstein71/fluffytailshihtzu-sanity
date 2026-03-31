defd96a57f1eec0e217699f84b5248d515c926ab
jsnfeinstein71 <jsnfeinstein71@gmail.com>
2026-03-31 14:54:16 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default async function HomePage() {
 
   return (
     <main className="wrap">
-      <div className="nav" style={{marginBottom: '16px'}}>
+      <div className="nav navPageLinks" style={{marginBottom: '16px'}}>
         <a className="btn" href="/about">About</a>
         <a className="btn" href="/available-puppies">Available Puppies</a>
         <a className="btn" href="/upcoming-litters">Upcoming Litters</a>
@@ -65,7 +65,7 @@ export default async function HomePage() {
             : 'Waitlist open'}
         </div>
 
-        <div className="nav">
+        <div className="nav navActions">
           <WaitlistModal
             waitlistUrl={waitlistUrl}
             buttonLabel="Join the Waitlist"
