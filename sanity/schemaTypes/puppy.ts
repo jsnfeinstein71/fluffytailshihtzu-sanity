6c06c06df8338434921782ab2ae8c5e931a88981
jsnfeinstein71 <jsnfeinstein71@gmail.com>
2026-04-02 14:26:51 -0500

    Update puppy.ts

--- sanity/schemaTypes/puppy.ts
+++ sanity/schemaTypes/puppy.ts
@@ -12,6 +12,16 @@ export const puppyType = defineType({
       validation: (Rule) => Rule.required(),
     }),
     defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'litter',
       title: 'Litter',
       type: 'reference',
