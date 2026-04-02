eacfdf62323db81dc8e5d20bfdd9a680329186e1
jsnfeinstein71 <jsnfeinstein71@gmail.com>
2026-04-02 16:45:03 -0500

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,11 +2,12 @@ import {siteSettingsType} from './siteSettings'
 import {litterType} from './litter'
 import {puppyType} from './puppy'
 import {faqType} from './faq'
+import {dogType} from './dog'
 
 export const schemaTypes = [
   siteSettingsType,
   litterType,
   puppyType,
   faqType,
+  dogType,
 ]
-
