0cabb01437cef2bd27376902974c48ead9cf9412
jsnfeinstein71 <jsnfeinstein71@gmail.com>
2026-04-05 13:59:24 -0500

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,1 +1,19 @@
+import {siteSettingsType} from './siteSettings'
+import {litterType} from './litter'
+import {puppyType} from './puppy'
+import {faqType} from './faq'
+import {smsMessageType} from './smsMessage'
+import {puppyInquiryType} from './puppyInquiry'
+import {paymentRecordType} from './paymentRecord'
+import {depositLinkType} from './depositLink'
 
+export const schemaTypes = [
+  siteSettingsType,
+  litterType,
+  puppyType,
+  faqType,
+  smsMessageType,
+  puppyInquiryType,
+  paymentRecordType,
+  depositLinkType,
+]
