36fb124f82fd7cbfc5daa8519255c5dc75a33b7c
jsnfeinstein71 <jsnfeinstein71@gmail.com>
2026-04-05 13:28:44 -0500

    Create page.tsx

---
+++ app/pay/[token]/page.tsx
@@ -0,0 +1,30 @@
+import {redirect, notFound} from 'next/navigation'
+import {client} from '@/sanity/lib/client'
+
+export const revalidate = 0
+
+type DepositLink = {
+  stripeCheckoutUrl?: string
+  active?: boolean
+}
+
+const depositLinkQuery = `*[_type == "depositLink" && token == $token][0]{
+  stripeCheckoutUrl,
+  active
+}`
+
+export default async function DepositRedirectPage({
+  params,
+}: {
+  params: Promise<{token: string}>
+}) {
+  const {token} = await params
+
+  const link = await client.fetch<DepositLink | null>(depositLinkQuery, {token})
+
+  if (!link || !link.active || !link.stripeCheckoutUrl) {
+    notFound()
+  }
+
+  redirect(link.stripeCheckoutUrl)
+}
